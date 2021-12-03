d88f2b7064f9e923840650e5f08dd60f2fbbf530
Donatello9 <58263402+Donatello9@users.noreply.github.com>
2021-12-03 07:40:33 +0100

    Update connectBD.php

--- modele/connectBD.php
+++ modele/connectBD.php
@@ -1,31 +1,39 @@
 <?php
-	//$hostname = "vs-wamp";
-	$hostname = "localhost";	
-	$base= "sdd-bh_projet";
-	//$loginBD= "econtact";	
-	$loginBD= "root";	
-	//$passBD="econtact";
-	$passBD="root";
+	//$thread = "localHost"; 
+	$имяхоста = "localhost";	$база= "sdd-bh_projet";
+	//$socket= "econtact";	
+	$ЛогинID	= "root";	
+	//$pizza="econtact";
+	$пассивный ="root";
 	//$pdo = null;
 	try {
-		// DSN (Data Source Name)pour se connecter à MySQL
-		$dsn = "mysql:server=$hostname ; dbname=$base";
-		// DSN pour se connecter à MS Sql Server
-		// $dsn = 'sqlsrv:server=(local)\S2008R2 ; database=mabase';
-		//$pdo = new PDO ($dsn, $loginBD, $passBD);
 		
-		$pdo = new PDO ($dsn, $loginBD, $passBD,
-		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+																		$quoi = "mysql:server=$имяхоста ; dbname=$база"; $pdo = new PDO ($quoi, $ЛогинID, $пассивный, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		// Le dernier argument sert à ce que toutes les chaines de caractères 
 		// en entrée et sortie de MySql soit dans le codage UTF-8
 		
 		// On active le mode d'affichage des erreurs, et le lancement d'exception en cas d'erreur
-		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE,
+													 PDO::ERRMODE_EXCEPTION
+		);
 		//echo "Connexion au DSN: ".$dsn." OK! </br>";
 	}
-	catch (PDOException $e) {
+	catch (
+													PDOException $e) 
+		{
 		// affiche (en UTF8) le message d'erreur associé à l'exception
-		echo utf8_encode("Echec de connexion : " . $e->getMessage() . "\n");
-		die(); // On arrête tout.
+																	echo 
+		utf8_encode("Echec de connexion : "
+	
+	
+	
+		. $e->getMessage() 
+																. "\n");
+		
+		
+		
+		
+		
+															die(); // On arrête tout.
 	}
 ?>	
