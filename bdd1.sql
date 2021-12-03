14dfccf59887ad1779a0ffd4b1e2d4186633a1e8
MikeOxmoll <77077050+MikeOxmoll@users.noreply.github.com>
2021-12-03 05:38:47 +0100

    Update bdd1.sql

--- bdd1.sql
+++ bdd1.sql
@@ -1,4 +1,31 @@
-CREATE TABLE Personne(
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Lun 08 Novembre 2021 à 01:39
+-- Version du serveur :  5.7.11
+-- Version de PHP :  7.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `sdd-bh_projet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+CREATE OR REPLACE TABLE Personne(
    IdPersonne INT,
    Nom VARCHAR(50),
    Prenom VARCHAR(50),
@@ -9,20 +36,20 @@ CREATE TABLE Personne(
    PRIMARY KEY(IdPersonne)
 );
 
-CREATE TABLE Sauve(
+CREATE OR REPLACE TABLE Sauve(
    IdPersonne INT,
    PRIMARY KEY(IdPersonne),
    FOREIGN KEY(IdPersonne) REFERENCES Personne(IdPersonne)
 );
 
-CREATE TABLE Sauveteur(
+CREATE OR REPLACE TABLE Sauveteur(
    idSauveteur INT,
    IdPersonne INT NOT NULL,
    PRIMARY KEY(idSauveteur),
    FOREIGN KEY(IdPersonne) REFERENCES Personne(IdPersonne)
 );
 
-CREATE TABLE Utilisateur(
+CREATE OR REPLACE TABLE Utilisateur(
    IdUser INT,
    pseudo VARCHAR(50) NOT NULL,
    Nom VARCHAR(50),
@@ -33,31 +60,31 @@ CREATE TABLE Utilisateur(
    UNIQUE(email)
 );
 
-CREATE TABLE Sauvetage(
+CREATE OR REPLACE TABLE Sauvetage(
    idSauvetage VARCHAR(50),
    DateSautage DATE,
    Description TEXT,
    PRIMARY KEY(idSauvetage)
 );
 
-CREATE TABLE Bateau(
+CREATE OR REPLACE TABLE Bateau(
    idBateau INT,
    Station VARCHAR(50),
    PRIMARY KEY(idBateau)
 );
 
-CREATE TABLE Decoration(
+CREATE OR REPLACE TABLE Decoration(
    Intitule VARCHAR(50),
    PRIMARY KEY(Intitule)
 );
 
-CREATE TABLE Admin(
+CREATE OR REPLACE TABLE Admin(
    IdUser INT,
    PRIMARY KEY(IdUser),
    FOREIGN KEY(IdUser) REFERENCES Utilisateur(IdUser)
 );
 
-CREATE TABLE decorer(
+CREATE OR REPLACE TABLE decorer(
    idSauveteur INT,
    Intitule VARCHAR(50),
    PRIMARY KEY(idSauveteur, Intitule),
@@ -65,7 +92,7 @@ CREATE TABLE decorer(
    FOREIGN KEY(Intitule) REFERENCES Decoration(Intitule)
 );
 
-CREATE TABLE assite(
+CREATE OR REPLACE TABLE assite(
    idSauveteur INT,
    idSauvetage VARCHAR(50),
    PRIMARY KEY(idSauveteur, idSauvetage),
@@ -73,7 +100,7 @@ CREATE TABLE assite(
    FOREIGN KEY(idSauvetage) REFERENCES Sauvetage(idSauvetage)
 );
 
-CREATE TABLE concerne(
+CREATE OR REPLACE TABLE concerne(
    IdPersonne INT,
    idSauvetage VARCHAR(50),
    PRIMARY KEY(IdPersonne, idSauvetage),
@@ -81,7 +108,7 @@ CREATE TABLE concerne(
    FOREIGN KEY(idSauvetage) REFERENCES Sauvetage(idSauvetage)
 );
 
-CREATE TABLE demandeSauvetage(
+CREATE OR REPLACE TABLE demandeSauvetage(
    IdUser INT,
    idSauvetage VARCHAR(50),
    PRIMARY KEY(IdUser, idSauvetage),
@@ -89,7 +116,7 @@ CREATE TABLE demandeSauvetage(
    FOREIGN KEY(idSauvetage) REFERENCES Sauvetage(idSauvetage)
 );
 
-CREATE TABLE bateauSauvetage(
+CREATE OR REPLACE TABLE bateauSauvetage(
    idSauvetage VARCHAR(50),
    idBateau INT,
    PRIMARY KEY(idSauvetage, idBateau),
@@ -97,7 +124,7 @@ CREATE TABLE bateauSauvetage(
    FOREIGN KEY(idBateau) REFERENCES Bateau(idBateau)
 );
 
-CREATE TABLE demandePersonne(
+CREATE OR REPLACE TABLE demandePersonne(
    IdPersonne INT,
    IdUser INT,
    PRIMARY KEY(IdPersonne, IdUser),
